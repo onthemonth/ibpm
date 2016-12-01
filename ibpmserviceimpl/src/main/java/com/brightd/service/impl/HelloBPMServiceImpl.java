9919f90a6acdb3aa93cc440da27310d60f69c504
ppengkkang <45534469@qq.com>
2016-12-01 18:24:28 +0800

    -

--- ibpmserviceimpl/src/main/java/com/brightd/service/impl/HelloBPMServiceImpl.java
+++ ibpmserviceimpl/src/main/java/com/brightd/service/impl/HelloBPMServiceImpl.java
@@ -3,11 +3,15 @@ package com.brightd.service.impl;
 import com.brightd.service.HelloBPMService;
 import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
 import org.jbpm.kie.services.impl.ProcessServiceImpl;
+import org.jbpm.kie.services.impl.RuntimeDataServiceImpl;
+import org.jbpm.kie.services.impl.bpmn2.BPMN2DataServiceImpl;
 import org.jbpm.services.api.DeploymentService;
 import org.jbpm.services.api.model.DeployedUnit;
 import org.jbpm.services.api.model.DeploymentUnit;
+import org.jbpm.shared.services.impl.TransactionalCommandService;
 import org.kie.api.KieBase;
 import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.task.TaskService;
 import org.kie.internal.io.ResourceFactory;
 import org.kie.internal.runtime.conf.RuntimeStrategy;
 import org.kie.internal.utils.KieHelper;
@@ -22,8 +26,20 @@ import java.util.Map;
 public class HelloBPMServiceImpl implements HelloBPMService{
 
     public final String GROUP_ID = "com.brightd";
-    public final String ARTIFACT_ID = "mykjar";
+    public final String ARTIFACT_ID = "km";
     public final String VERSION = "1.0-SNAPSHOT";
+
+    @Resource
+    private TaskService taskService;
+
+    @Resource
+    private TransactionalCommandService transactionCmdService;
+
+    @Resource
+    private RuntimeDataServiceImpl runtimeDataService;
+
+    @Resource
+    private BPMN2DataServiceImpl definitionService;
 
     @Resource
     private DeploymentService deploymentService;
@@ -41,11 +57,12 @@ public class HelloBPMServiceImpl implements HelloBPMService{
         deploymentUnit.setStrategy(RuntimeStrategy.PER_REQUEST);
         deploymentService.deploy(deploymentUnit);
 
+        RuntimeManager runtimeManager = deploymentService.getRuntimeManager(deploymentUnit.getIdentifier());
+
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("customer_phone", "4444-55555");
         //long processInstance = processService.startProcess("CustomersRelationship.customers", "C001");
         long processInstance = processService.startProcess(deploymentUnit.getIdentifier(),"CustomersRelationship.customers",params);
-
 
         return "Hello BPM "+str;
     }
---
+++ km/src/main/java/com/brightd/App.java
@@ -0,0 +1,10 @@
+package com.brightd;
+
+/**
+ * Created by pengyong on 16/12/1.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("km");
+    }
+}
